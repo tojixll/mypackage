7662c9c292bc82d2643258782fe7788dff8362dd
tojixll <kachokp45@gmail.com>
2026-04-19 19:56:17 +0500

    Add files via upload

---
+++ mypackage/cmd/app/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+
+	"mypackage.go/mypackage"
+)
+
+func main() {
+    if sum := mypackage.Add(1, 2); sum != 3 {
+        panic(fmt.Sprintf("sum expected to be 3; got %d", sum))
+    }
+
+    fmt.Println("Well done!")
+}
---
+++ mypackage/go.mod
@@ -0,0 +1,5 @@
+module mypackage.go
+
+go 1.26.2
+
+require github.com/tojixll/mypackage v0.0.0-20260419135235-ad40b1f9ad37
